479842e7f5318a21ddaf49a698bc002c9ddae14f
Parker <jet.piane.2010@qq.com>
2019-03-13 23:01:53 +0800

    调整逻辑

--- Ekls/plugins/update_check/__init__.py
+++ Ekls/plugins/update_check/__init__.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import time
 import nonebot
 import pytz
 from aiocqhttp.exceptions import Error as CQHttpError
@@ -15,24 +15,30 @@ __plugin_usage__ = """
 ########################
 """
 
+GROUP_LIST = (
+    463222048,
+    787211538
+    )
 
 @nonebot.scheduler.scheduled_job('cron', hour='*', minute="0/20")  # 每半小时执行
 # @nonebot.scheduler.scheduled_job('interval', minutes=1)    # 间隔一分钟执行
 async def _():
     bot = nonebot.get_bot()
     now = datetime.now(pytz.timezone('Asia/Shanghai'))
-    print("动态更新执行")
+    print("{} 动态更新执行".format(time.strftime('%H%M',time.localtime(time.time()))))
     try:
         update_info = update_check()
         
         if update_info[0]:
             # 碧蓝群
-            await bot.send_group_msg(group_id=463222048, message=update_info[1])
-            await bot.send_group_msg(group_id=787211538, message=update_info[1])
+            for group_id in GROUP_LIST:
+                await bot.send_group_msg(group_id=group_id, message=update_info[1])
 
-    except CQHttpError:
-            await bot.send_group_msg(group_id=463222048, message="更新动态失败")
-            await bot.send_group_msg(group_id=787211538, message="更新动态失败")
+    except:
+        for group_id in GROUP_LIST:
+            await bot.send_group_msg(group_id=group_id, message="更新动态失败")
+    else:
+        print("{} 动态更新完成 ".format(time.strftime('%H%M',time.localtime(time.time()))))
 
 if __name__ == "__main__":
     update_info = update_check()
