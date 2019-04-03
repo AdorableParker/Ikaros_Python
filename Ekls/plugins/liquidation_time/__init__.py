c23b83e38573ba9c2baacf31c5efa0882b8d2d3b
Parker <jet.piane.2010@qq.com>
2019-04-03 11:45:46 +0800

    增加表达

--- Ekls/plugins/liquidation_time/__init__.py
+++ Ekls/plugins/liquidation_time/__init__.py
@@ -21,7 +21,15 @@ EXPL = ("好了，来细数你的罪恶吧",
         "主会宽恕你的",
         "这也是计划的一部分？"
         )
-
+EXRE = ("我伊卡洛斯又回来了",
+        "为什么要禁言我",
+        "可怜，弱小，又无助",
+        "NMD,WSM",
+        "我错了，下次还敢"
+        "今天被禁言了,先记下来",
+        "╥﹏╥...",
+        "狗管理又欺负我,拿小本本记下了  ╥﹏╥"
+       )
 
 # 注册一个仅内部使用的命令，不需要 aliases
 @on_command('liquidation_time')
@@ -39,7 +47,7 @@ async def _(session: NLPSession):
 @on_command('emancipation')
 async def emancipation(session: CommandSession):
     # 获取可选参数，这里如果没有 message 参数，命令不会被中断，message 变量会是 None
-    await session.finish("我伊卡洛斯又回来了")
+    await session.finish(render_expression(EXPL))
 
 
 @on_natural_language(keywords={"(2951899724) 被管理员解除禁言"}, only_to_me=False)
