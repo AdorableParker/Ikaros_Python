a66513674ff730545e961d8cbf6bf9d345717641
Parker <jet.piane.2010@qq.com>
2019-05-10 08:31:33 +0800

    update .gitignore

--- Ekls/plugins/tool/shorten_url.py
+++
@@ -1,7 +0,0 @@
-import json
-import requests
-
-def shorten_url(url):
-    short_link_api = "http://mrw.so/api.php?format=json&url={}".format(url)
-    short_link =  json.loads(requests.get(short_link_api).text)
-    return short_link["url"]
