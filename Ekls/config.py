ef2ce61fe2167b32e7ddaf70fe9df6291389fe39
Parker <jet.piane.2010@qq.com>
2019-05-13 16:43:00 +0800

    尝试使用VSCode

--- Ekls/config.py
+++ Ekls/config.py
@@ -7,7 +7,6 @@ MAX_VALIDATION_FAILURES = 3 # 参数最大错误次数
 DEBUG = False   # 调试模式
 SESSION_RUNNING_EXPRESSION = "我现在忙着呢，等下再来"   # 已有命令会话运行时回复语
 SESSION_CANCEL_EXPRESSION = "命令已取消" # 命令会话取消运行时回复语
-TOO_MANY_VALIDATION_FAILURES_EXPRESSION = ('你输错太多次啦，需要的时候再叫我吧', '你输错太多次了，建议先看看使用帮助哦～', '你输错太多次了，使用help命令查看使用帮助哦～')  # 参数错误次数过多时回复语
 #-----------------------------------------------
 HOST = '127.0.0.1'
 PORT = 1234
