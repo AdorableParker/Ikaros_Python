a4e8034de96d4ab03b759313a6e6fce7aa07202f
Parker <jet.piane.2010@qq.com>
2019-06-25 19:42:56 +0800

    停用已经失效的短网址服务

--- Ekls/plugins/tool/shorten_url.py
+++ Ekls/plugins/tool/shorten_url.py
@@ -1,7 +1,11 @@
 import json
 import requests
+import random
 
 def shorten_url(url):
-    short_link_api = "http://mrw.so/api.php?format=json&url={}".format(url)
-    short_link =  json.loads(requests.get(short_link_api).text)
-    return short_link["url"]
+
+    return url
+
+    #short_link_api = "http://mrw.so/api.php?format=json&url={}".format(url)
+    #short_link =  json.loads(requests.get(short_link_api).text)
+    #return short_link["url"]
