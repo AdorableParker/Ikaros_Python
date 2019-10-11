884a8a5534c956826a29b4b815ca44803d0b456b
Parker <jet.piane.2010@qq.com>
2019-10-11 10:37:01 +0800

    更改api以解决运行堵塞的问题

--- Ekls/plugins/Remote.py
+++ Ekls/plugins/Remote.py
@@ -1,9 +1,9 @@
 from nonebot import on_command, CommandSession
 from nonebot.permission import SUPERUSER
-from os import system
+from os import startfile
 
 
 @on_command('remote', aliases=['启动远程',], only_to_me=False, permission=SUPERUSER)
 async def _(session: CommandSession):
-    system("TeamViewer.exe")
+    startfile("TeamViewer.exe")
     await session.finish("命令已执行")
