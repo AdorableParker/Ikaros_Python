68587131bc3baf6c3a446dff307e96f0990ef5e6
AdorableParker <jet.piane.2010@qq.com>
2019-10-23 10:09:40 +0800

    解除屏蔽，保存修改

--- Ekls/plugins/Remote.py
+++ Ekls/plugins/Remote.py
@@ -6,5 +6,5 @@ import nonebot
 
 @on_command('remote', aliases=['启动远程',], only_to_me=False, permission=SUPERUSER)
 async def _(session: CommandSession):
-    # startfile("TeamViewer.exe")
+    startfile("TeamViewer.exe")
     await session.finish("命令已执行")
